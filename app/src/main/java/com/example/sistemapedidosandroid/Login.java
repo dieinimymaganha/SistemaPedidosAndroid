2b705c9a62da8be9d56fd3e9f9480c2d810104a2
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-16 08:22:00 -0300

    Incluindo direcionamento para cadastrar usuário

--- app/src/main/java/com/example/sistemapedidosandroid/Login.java
+++ app/src/main/java/com/example/sistemapedidosandroid/Login.java
@@ -2,13 +2,28 @@ package com.example.sistemapedidosandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button login, cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cadastrar = findViewById(R.id.cadastrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, CadastrarUsuario.class);
+                startActivity(i);
+            }
+        });
     }
 }
