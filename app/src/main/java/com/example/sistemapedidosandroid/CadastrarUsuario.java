bd824b69322d9262d038c7c11069ff5f9bf28ef6
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-16 10:46:05 -0300

    Criando chamada para envio das informações para API

--- app/src/main/java/com/example/sistemapedidosandroid/CadastrarUsuario.java
+++ app/src/main/java/com/example/sistemapedidosandroid/CadastrarUsuario.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sistemapedidosandroid.modelo.Usuario;
+import com.example.sistemapedidosandroid.retrofit.RetrofitInicializador;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CadastrarUsuario extends AppCompatActivity {
 
@@ -21,12 +29,44 @@ public class CadastrarUsuario extends AppCompatActivity {
         usuario = findViewById(R.id.usuario);
         email = findViewById(R.id.email);
         senha = findViewById(R.id.senha);
+        cadastrar = findViewById(R.id.cadastrar);
 
         Usuario user = new Usuario();
 
-        user.setUsuario(usuario.getText().toString());
-        user.setEmail(email.getText().toString());
-        user.setSenha(senha.getText().toString());
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user.setUsername(usuario.getText().toString());
+                user.setEmail(email.getText().toString());
+                user.setPassword(senha.getText().toString());
+
+                Call call = new RetrofitInicializador().getUsuarioService().cadastrar(user);
+
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        Log.i("onResponse", "Requisição com sucesso");
+                    }
+
+                    @Override
+                    public void onFailure(Call call, Throwable t) {
+
+                        Log.e("onFailure","Requisão falhou");
+
+                    }
+                });
+
+                Toast.makeText(CadastrarUsuario.this,"Usuario " + user.getUsername()
+                + "Salvo!", Toast.LENGTH_SHORT).show();
+
+                finish();
+
+            }
+        });
+
+
 
     }
 }
