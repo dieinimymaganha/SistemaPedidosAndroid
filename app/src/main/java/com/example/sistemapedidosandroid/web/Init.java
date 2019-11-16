277f4e94aa35f397ec372f1b46248cd9026cb652
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-16 11:19:23 -0300

    Criando activity e classe para pagina inicial

---
+++ app/src/main/java/com/example/sistemapedidosandroid/web/Init.java
@@ -0,0 +1,16 @@
+package com.example.sistemapedidosandroid.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sistemapedidosandroid.R;
+
+public class Init extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
