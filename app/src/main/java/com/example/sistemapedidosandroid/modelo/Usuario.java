12b5ff152fd9f29d124c29b740d99ce347826a57
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-16 08:37:54 -0300

    Criada Classe Usuario

---
+++ app/src/main/java/com/example/sistemapedidosandroid/modelo/Usuario.java
@@ -0,0 +1,43 @@
+package com.example.sistemapedidosandroid.modelo;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private Long id;
+    private String usuario;
+    private String email;
+    private String senha;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
