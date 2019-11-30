95fb830b485f91c860c46e05f9144a15e47ec5d3
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-30 10:16:47 -0300

    Criado metodo PUT para cliente

--- backend/src/main/java/backend/backend/controller/ClienteController.java
+++ backend/src/main/java/backend/backend/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,20 +27,33 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	
 	@GetMapping
-	List<Cliente> all(){
+	List<Cliente> all() {
 		return clienteRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	Cliente one(@PathVariable Long id) {
 		return clienteRepository.findById(id).orElseThrow(() -> new ClienteNotFoundException(id));
 	}
-	
+
 	@PostMapping
 	Cliente newCliente(@RequestBody Cliente newCliente) {
 		return clienteRepository.save(newCliente);
+	}
+
+	@PutMapping("/{id}")
+	Cliente replaceCliente(@RequestBody Cliente newCliente, @PathVariable Long id) {
+		return clienteRepository.findById(id).map(cliente -> {
+			cliente.setNome(newCliente.getNome());
+			cliente.setSobrenome(newCliente.getSobrenome());
+			cliente.setCpf(newCliente.getCpf());
+			return clienteRepository.save(cliente);
+		}).orElseGet(() -> {
+			newCliente.setId(id);
+			return clienteRepository.save(newCliente);
+		});
+
 	}
 
 }
