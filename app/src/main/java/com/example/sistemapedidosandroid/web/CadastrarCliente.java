b61c0db21e346fa195fc4882eab2d465f47690f3
Dieinimy Maganha <dieinimy@gmail.com>
2019-12-01 12:47:09 -0300

    Melhorias cadastro cliente
    * Tornado campos obrigatorios
    * Mensagem de CPF já cadastrado

--- app/src/main/java/com/example/sistemapedidosandroid/web/CadastrarCliente.java
+++ app/src/main/java/com/example/sistemapedidosandroid/web/CadastrarCliente.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 import com.example.sistemapedidosandroid.R;
 import com.example.sistemapedidosandroid.modelo.ClienteModel;
 import com.example.sistemapedidosandroid.retrofit.RetrofitInicializador;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,44 +34,80 @@ public class CadastrarCliente extends AppCompatActivity {
         edSobrenome = findViewById(R.id.sobrenome);
         edCpf = findViewById(R.id.cpf);
         btCadastrar = findViewById(R.id.cadastrar);
+        btCancelar = findViewById(R.id.cancelar);
+
+
+        // Criando mascara para cpf
+        SimpleMaskFormatter smf = new SimpleMaskFormatter("NNN.NNN.NNN-NN");
+        MaskTextWatcher mtw = new MaskTextWatcher(edCpf, smf);
+        edCpf.addTextChangedListener(mtw);
+        // Fim mascara
 
 
         btCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String nome = edNome.getText().toString();
+                String sobrenome = edSobrenome.getText().toString();
+                String cpf = edCpf.getText().toString();
+
+
                 ClienteModel cliente = new ClienteModel();
+                cliente.setNome(nome);
+                cliente.setSobrenome(sobrenome);
+                cliente.setCpf(cpf);
 
-                cliente.setNome(edNome.getText().toString());
-                cliente.setSobrenome(edSobrenome.getText().toString());
-                cliente.setCpf(edCpf.getText().toString());
+                if (nome == null || nome.equals("")) {
+                    edNome.setError("Este campo é obrigatório");
+                }
 
-                Call call = new RetrofitInicializador().getClienteService().cadastrar(cliente);
+                if (sobrenome == null || sobrenome.equals("")) {
 
-                call.enqueue(new Callback() {
-                    @Override
-                    public void onResponse(Call call, Response response) {
+                    edSobrenome.setError(("Este campo é obrigatório"));
+                }
+                if (cpf == null || cpf.equals("")) {
 
-                        int resposta = response.code();
+                    edCpf.setError("Este Campo é obrigatório");
+                } else {
+                    Call call = new RetrofitInicializador().getClienteService().cadastrar(cliente);
+
+                    call.enqueue(new Callback() {
+                        @Override
+                        public void onResponse(Call call, Response response) {
+
+                            int resposta = response.code();
 
 
-                        if (resposta == 500) {
-                            Toast.makeText(CadastrarCliente.this, "CPF " + cliente.getCpf() + " já cadastrado", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.i("onResponse", "Requisição com sucesso");
-                            Toast.makeText(CadastrarCliente.this, "Cliente " + cliente.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(CadastrarCliente.this, ListarCliente.class);
-                            startActivity(i);
+                            if (resposta == 500) {
+                                edCpf.setError("CPF já cadastrado");
+                            } else {
+                                Log.i("onResponse", "Requisição com sucesso");
+                                Toast.makeText(CadastrarCliente.this, "Cliente " + cliente.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(CadastrarCliente.this, ListarCliente.class);
+                                startActivity(i);
+
+                            }
 
                         }
 
-                    }
+                        @Override
+                        public void onFailure(Call call, Throwable t) {
+                            Log.e("onFailure", "Requisão falhou");
+                        }
+                    });
 
-                    @Override
-                    public void onFailure(Call call, Throwable t) {
-                        Log.e("onFailure", "Requisão falhou");
-                    }
-                });
+                }
 
+
+            }
+        });
+
+        btCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CadastrarCliente.this, Cliente.class);
+                startActivity(i);
             }
         });
 
