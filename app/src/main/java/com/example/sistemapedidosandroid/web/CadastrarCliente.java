9b41eb3c2752ffa8a42a794d15c79eafa76ef2f7
Dieinimy Maganha <dieinimy@gmail.com>
2019-12-01 11:14:27 -0300

    Criado redirecionamento após cadastrar o cliente e exibida mensagem de cliente já cadastrado

--- app/src/main/java/com/example/sistemapedidosandroid/web/CadastrarCliente.java
+++ app/src/main/java/com/example/sistemapedidosandroid/web/CadastrarCliente.java
@@ -2,11 +2,13 @@ package com.example.sistemapedidosandroid.web;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sistemapedidosandroid.R;
 import com.example.sistemapedidosandroid.modelo.ClienteModel;
@@ -32,7 +34,6 @@ public class CadastrarCliente extends AppCompatActivity {
         btCadastrar = findViewById(R.id.cadastrar);
 
 
-
         btCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,12 +48,25 @@ public class CadastrarCliente extends AppCompatActivity {
                 call.enqueue(new Callback() {
                     @Override
                     public void onResponse(Call call, Response response) {
-                        Log.i("onResponse", "Requisição com sucesso");
+
+                        int resposta = response.code();
+
+
+                        if (resposta == 500) {
+                            Toast.makeText(CadastrarCliente.this, "CPF " + cliente.getCpf() + " já cadastrado", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.i("onResponse", "Requisição com sucesso");
+                            Toast.makeText(CadastrarCliente.this, "Cliente " + cliente.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(CadastrarCliente.this, ListarCliente.class);
+                            startActivity(i);
+
+                        }
+
                     }
 
                     @Override
                     public void onFailure(Call call, Throwable t) {
-                        Log.e("onFailure","Requisão falhou");
+                        Log.e("onFailure", "Requisão falhou");
                     }
                 });
 
