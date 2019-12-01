6a9da60ccdc6be4e0ef8035da5dec4f7808784a6
Dieinimy Maganha <dieinimy@gmail.com>
2019-12-01 10:17:41 -0300

    Criando metodo para achar pedidos de 1 cliente

--- backend/src/main/java/backend/backend/repository/PedidoRepository.java
+++ backend/src/main/java/backend/backend/repository/PedidoRepository.java
@@ -1,9 +1,13 @@
 package backend.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import backend.backend.model.Pedido;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByCliente_Id(Long cliente_id);
 
 }
