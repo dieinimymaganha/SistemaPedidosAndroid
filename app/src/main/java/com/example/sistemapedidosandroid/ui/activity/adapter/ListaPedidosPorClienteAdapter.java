97495fdfe3dc1660b8a73f972f9e43bb861c22ed
Dieinimy Maganha <dieinimy@gmail.com>
2019-12-10 00:19:22 -0300

    Melhorando o visual dos pedidos dos clientes

--- app/src/main/java/com/example/sistemapedidosandroid/ui/activity/adapter/ListaPedidosPorClienteAdapter.java
+++ app/src/main/java/com/example/sistemapedidosandroid/ui/activity/adapter/ListaPedidosPorClienteAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.sistemapedidosandroid.R;
+import com.example.sistemapedidosandroid.modelo.ItemPedido;
 import com.example.sistemapedidosandroid.modelo.Pedido;
 
 import java.util.List;
@@ -42,22 +43,24 @@ public class ListaPedidosPorClienteAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         Pedido pedido = pedidos.get(position);
         LayoutInflater inflater = LayoutInflater.from(context);
-
         View view = convertView;
         if (view == null) {
             view = inflater.inflate(R.layout.itens_pedidos_por_cliente, parent, false);
-
         }
         criaDadosAdapter(pedido, view);
-
         return view;
     }
 
     private void criaDadosAdapter(Pedido pedido, View view) {
+        List<ItemPedido> itens = pedido.getItempedido();
+        TextView produto = view.findViewById(R.id.itens_pedido_cliente_produto);
+        TextView quantidade = view.findViewById(R.id.itens_pedido_cliente_quantidade);
+        for (ItemPedido item : itens) {
+            produto.setText("Produto: " + item.getProduto().toString());
+            quantidade.setText("Quantidade: " + item.getQuantidade());
+        }
         TextView numeroPedido = view.findViewById(R.id.itens_pedido_cliente_id);
         numeroPedido.setText("Numero do pedido: " + pedido.getId());
-        TextView produto = view.findViewById(R.id.itens_pedido_cliente_produto);
-        produto.setText(pedido.getItempedido().toString());
     }
 
 }
